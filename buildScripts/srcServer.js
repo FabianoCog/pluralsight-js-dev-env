84b73a0ed1c73d100775a55fac8e5a86ba828f4d
Fabiano Ferro <fabiano.ferro@cognizant.com>
2019-06-06 17:00:34 -0300

    ok

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import path from 'path';
-import open from 'open';
+import express from 'express'
+import path from 'path'
+import open from 'open'
 
 const port = 3000;
 const app = express();
